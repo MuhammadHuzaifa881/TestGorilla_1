1bbef2598509d6c4e54e4956da8e081fad487070
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2023-07-11 05:12:33 +0700

    Dashboard on working

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="main">
+    <div class="header">
+        <span class=""></span>
+    </div>
+</div>
+</template>
+<script>
+export default{
+    name:'Dashboard_gorilla'
+}
+</script>
+<style>
+.language_selector{
+font-weight: bold;
+position: relative;
+}
+</style>
--- src/components/router.js
+++ src/components/router.js
@@ -2,10 +2,16 @@ import {createRouter,createWebHistory} from 'vue-router';
 import Login from '../components/Login.vue'
 import Register from '../components/SignUp.vue'
 import ForgetScreen from '../components/ForgetScreen.vue'
+import Dashboard from '../components/Dashboard.vue'
 const routes=[
+    {
+        name:'Dashboard',
+        path:'/',
+        component:Dashboard
+    },
 {
     name:'Login',
-    path:'/',
+    path:'/Login',
     component:Login
 },
 {
