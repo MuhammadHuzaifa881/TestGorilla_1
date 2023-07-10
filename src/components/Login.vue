2f4b2241b6b6b2b8944d8c0a9d012d9d61d27844
MuhammadHuzaifa881 <huzaifanasir258@gmail.com>
2023-07-10 08:16:38 +0700

    Login screen

---
+++ src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+<h1 class="text-primary">hello login User</h1>
+</template>
+<script>
+export default {
+    name:"login_",
+}
+</script>
+<style scoped>
+/* h1{
+    color: red;
+} */
+</style>
