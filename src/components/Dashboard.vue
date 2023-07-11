9178852fd966a92565f7d2a0481ccfb0b018d90f
Shahzaib <shahzaib.khann.1999@gmail.com>
2023-07-11 15:13:01 +0500

    Updated the Dashboard and add some Navbar Elements

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,14 +1,95 @@
 <template>
-<div class="main">
-    <div class="header">
-        <span class=""></span>
+    <div class="main">
+      <!-- Language Nav bar + Log in -->
+      <nav class="navbar px-5" style="position: fixed; top: 0; width: 100%; background-color: lightgrey">
+        <div class="container-fluid d-flex justify-content-between align-items-center">
+          <div class="d-flex">
+            <a class="navbar-brand" href="#">
+              <img src="https://www.testgorilla.com/wp-content/themes/testgorilla/assets/images/world.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" />
+            </a>
+            <div class="dropdown">
+              <a class="btn btn-secondary dropdown-toggle language" type="button" id="languageDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                EN
+              </a>
+              <!-- Drop menu items -->
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Arabic</a></li>
+                <li><a class="dropdown-item" href="#">English</a></li>
+                <li><a class="dropdown-item" href="#">French</a></li>
+                <li><a class="dropdown-item" href="#">Dutch</a></li>
+                <li><a class="dropdown-item" href="#"></a></li>
+                <li><a class="dropdown-item" href="#">French</a></li>
+                <li><a class="dropdown-item" href="#">French</a></li>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <router-link class="text-decoration-none" to="/Login">Log in</router-link>
+          </div>
+        </div>
+      </nav>
+      <!-- New Header content -->
+      <!-- Logo + nav items -->
+      <header class="bg-primary nav-header w-100" style="position: fixed; top: 56px">
+        <div class="container-fluid d-flex justify-content-center bg-secondary pad-remover">
+          <nav class="navbar navbar-expand-lg bg-body-tertiary w-100 px-5">
+            <div class="container-fluid">
+              <!-- Logo Test Gorilla -->
+              <a class="navbar-brand" href="#">
+                <img src="https://www.testgorilla.com/wp-content/uploads/2020/01/testgorilla-logo.png" style="width: 200px; height: 65px" />
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <!-- Nav menu items -->
+              <div class="collapse navbar-collapse float-end navbar-text">
+                <ul class="navbar-nav text-center">
+                  <li class="nav-item ml-5">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item ml-5">
+                    <a class="nav-link" href="#">Features</a>
+                  </li>
+                  <li class="nav-item ml-5">
+                    <a class="nav-link" href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Dropdown link
+                    </a>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
     </div>
-</div>
-</template>
-<script>
-export default{
-    name:'Dashboard_gorilla'
-}
-</script>
-<style>
-</style>
+  </template>
+  
+  <script>
+  import * as bootstrap from "bootstrap/dist/js/bootstrap.bundle";
+  export default {
+    name: "Dashboard_gorilla",
+    mounted() {
+      // Initialize Bootstrap dropdown functionality
+      new bootstrap.Dropdown(document.getElementById("languageDropdown"));
+    },
+  };
+  </script>
+  
+  <style>
+  .nav-adj {
+    justify-content: left !important;
+  }
+  .pad-remover {
+    padding-left: 0 !important ;
+    padding-right: 0 !important ;
+  }
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './components/router.js'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 createApp(App).use(Router).mount('#app')
